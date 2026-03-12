63fa18c888ec521bf5fdf6964a63beefcdf9afce
GeunhwaJeong <jeonggh892@gmail.com>
2026-03-12 22:37:26 +0900

    feat(haneul-kvstore): add BigTable table initialization script
    Automate creation of BigTable tables and column families with
    configured garbage collection policies for setting up new
    kvstore deployments.

---
+++ crates/haneul-kvstore/src/bigtable/init.sh
@@ -0,0 +1,22 @@
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+INSTANCE_ID=${1:-haneul}
+command=(
+  cbt
+  -instance
+  "$INSTANCE_ID"
+)
+if [[ -n $BIGTABLE_EMULATOR_HOST ]]; then
+  command+=(-project emulator)
+fi
+
+for table in checkpoints checkpoints_by_digest transactions objects epochs \
+    watermark_alt protocol_configs packages packages_by_id \
+    packages_by_checkpoint system_packages; do
+  (
+    set -x
+    "${command[@]}" createtable $table
+    "${command[@]}" createfamily $table haneul
+    "${command[@]}" setgcpolicy $table haneul maxversions=1
+  )
+done
